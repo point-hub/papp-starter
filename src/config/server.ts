19b565ea6d11d9bc0237ee86e7f70e46fae4e549
Martien Dermawan Tanama <martiendt@gmail.com>
2024-07-15 09:01:29 +0700

    feat: configure server port from env

---
+++ src/config/server.ts
@@ -0,0 +1,5 @@
+export const port: number = Number(import.meta.env.VITE_PORT)
+
+export default {
+  port: port
+}
--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,24 @@ import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), UnoCSS()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    plugins: [vue(), UnoCSS()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      port: Number(env.VITE_PORT)
+    },
+    preview: {
+      port: Number(env.VITE_PORT)
     }
-  }
-})
+  })
+}
